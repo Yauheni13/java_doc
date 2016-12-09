4aa84d8cf5e6de3257dfe06957cd85032b9110a1
Yauheni13 <yauheni.butsko@gmail.com>
2016-12-09 18:31:56 -0500

    replace function inside classes

--- sandbox_java/src/main/java/us/va/yauheni/MyFirstProgram.java
+++ sandbox_java/src/main/java/us/va/yauheni/MyFirstProgram.java
@@ -4,28 +4,22 @@ public class MyFirstProgram {
 
 	public static void main(String[] args){
 
-		double l = 6;
+		double l = 9;
 		Square s = new Square(l);
-		double a = 8.0;
+		double a = 7.0;
 		double b = 5.0;
 		Rectangle r = new Rectangle(a,b);
 		hello("user");
 		hello("world");
 		hello("Yauheni");
-		System.out.println("площадь квадрата со строной " + s.l + " = " + area(s));
-		System.out.println("площадь прямоугольника со сторонами " + r.a + " и " + r.b + " = " + area(r));
+		System.out.println("площадь квадрата со строной " + s.l + " = " + s.area());
+		System.out.println("площадь прямоугольника со сторонами " + r.a + " и " + r.b + " = " + r.area());
 }
 
 	public static void hello(String sombody){
 		System.out.println("Hello, " +  sombody + "!");
 	}
 
-	public static double area(Square s){
-		return s.l * s.l;
-	}
 
-	public static double area(Rectangle r){
-		return r.a * r.b;
-	}
 
 }
