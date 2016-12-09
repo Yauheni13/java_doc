c6882abf09255fc0abfff4d1e880bdb23317fcbb
Yauheni13 <yauheni.butsko@gmail.com>
2016-12-09 17:07:02 -0500

    added couple functions

--- sandbox_java/src/main/java/us/va/yauheni/MyFirstProgram.java
+++ sandbox_java/src/main/java/us/va/yauheni/MyFirstProgram.java
@@ -4,6 +4,21 @@ public class MyFirstProgram {
 
 	public static void main(String[] args){
 
-		System.out.println("Hello, world!");
+		hello("user");
+		hello("world");
+		hello("Yauheni");
+		double a = 8;
+		double b = 4;
+		System.out.println("площадь квадрата со строной " + a + "="+area(a));
+		System.out.println("площадь прямоугольника со стронами " + a + " и " + b + " = "+area(a, b));
 }
+	public static void hello(String sombody){
+		System.out.println("Hello, " +  sombody + "!");
+	}
+	public static double area(double l){
+		return l * l;
+	}
+	public static double area(double a, double b){
+		return a * b;
+	}
 }
