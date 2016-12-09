0e51dbe554731fb767995b53b176bff09e366298
Yauheni13 <yauheni.butsko@gmail.com>
2016-12-09 14:31:54 -0500

    added java.conspect

--- sandbox_java/src/main/java/us/va/yauheni/MyFirstProgram.java
+++ sandbox_java/src/main/java/us/va/yauheni/MyFirstProgram.java
@@ -3,6 +3,7 @@ package us.va.yauheni;
 public class MyFirstProgram {
 
 	public static void main(String[] args){
-System.out.println("Hello, world!");
+
+		System.out.println("Hello, world!");
 }
 }
