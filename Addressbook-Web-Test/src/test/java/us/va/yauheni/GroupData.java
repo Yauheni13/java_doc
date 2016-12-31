57f76cee655034522e5fdeeb60b6c573c3fddf19
Yauheni13 <yauheni.butsko@gmail.com>
2016-12-30 21:55:46 -0500

    refactoring a lot

---
+++ Addressbook-Web-Test/src/test/java/us/va/yauheni/GroupData.java
@@ -0,0 +1,25 @@
+package us.va.yauheni;
+
+public class GroupData {
+    private final String namegroup;
+    private final String header;
+    private final String footer;
+
+    public GroupData(String namegroup, String header, String footer) {
+        this.namegroup = namegroup;
+        this.header = header;
+        this.footer = footer;
+    }
+
+    public String getNamegroup() {
+        return namegroup;
+    }
+
+    public String getHeader() {
+        return header;
+    }
+
+    public String getFooter() {
+        return footer;
+    }
+}
---
+++ Addressbook-Web-Test/src/test/java/us/va/yauheni/TestBase.java
@@ -0,0 +1,93 @@
+package us.va.yauheni;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.NoAlertPresentException;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
+import java.util.concurrent.TimeUnit;
+
+public class TestBase {
+
+    FirefoxDriver wd;
+
+    public static boolean isAlertPresent(FirefoxDriver wd) {
+        try {
+            wd.switchTo().alert();
+            return true;
+        } catch (NoAlertPresentException e) {
+            return false;
+        }
+    }
+
+    @BeforeMethod
+    public void setUp() throws Exception {
+        wd = new FirefoxDriver();
+        wd.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+        wd.get("http://localhost/addressbook/");
+        login("admin", "secret");
+
+    }
+
+    private void login(String username, String password) {
+        wd.findElement(By.name("user")).click();
+        wd.findElement(By.name("user")).clear();
+        wd.findElement(By.name("user")).sendKeys(username);
+        wd.findElement(By.name("pass")).click();
+        wd.findElement(By.name("pass")).clear();
+        wd.findElement(By.name("pass")).sendKeys(password);
+        wd.findElement(By.xpath("//form[@id='LoginForm']/input[3]")).click();
+    }
+
+    private void logOut() {
+        wd.findElement(By.linkText("Logout")).click();
+    }
+
+    protected void returnToGroupPage() {
+        wd.findElement(By.linkText("group page")).click();
+    }
+
+    protected void submitGroupCreation() {
+        wd.findElement(By.name("submit")).click();
+    }
+
+    protected void fillGroupForm(GroupData groupData) {
+        wd.findElement(By.name("group_name")).click();
+        wd.findElement(By.name("group_name")).clear();
+        wd.findElement(By.name("group_name")).sendKeys(groupData.getNamegroup());
+        wd.findElement(By.name("group_header")).click();
+        wd.findElement(By.name("group_header")).clear();
+        wd.findElement(By.name("group_header")).sendKeys(groupData.getHeader());
+        wd.findElement(By.name("group_footer")).click();
+        wd.findElement(By.name("group_footer")).clear();
+        wd.findElement(By.name("group_footer")).sendKeys(groupData.getFooter());
+    }
+
+    protected void initGroupCreation() {
+        wd.findElement(By.name("new")).click();
+    }
+
+    protected void goToGroupPage() throws InterruptedException {
+            boolean boo = true;
+            wd.findElement(By.cssSelector(".admin>a")).click();
+            while(boo){
+            try {
+                wd.findElement(By.cssSelector("#content>h1"));
+                boo = false;
+            }
+            catch (Exception ex){
+                Thread.sleep(1000);
+                wd.findElement(By.cssSelector(".admin>a")).click();
+            }
+
+            }
+
+    }
+
+    @AfterMethod
+    public void tearDown() {
+        logOut();
+        wd.quit();
+    }
+}
