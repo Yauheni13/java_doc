b95e56f7a69a6b6f16654ba67a7eb77ac4d9ac79
Yauheni13 <yauheni.butsko@gmail.com>
2017-01-10 20:47:40 -0500

    new files

--- sandbox_java/src/test/java/us/va/yauheni/LambdaTest.java
+++ sandbox_java/src/test/java/us/va/yauheni/LambdaTest.java
@@ -7,19 +7,19 @@ public class LambdaTest {
         List<String> list = new ArrayList<String>();
         for (int i=0; i<10; i++)list.add(GenerateString.generateString(GenerateString.randomint(3,9)));
         for (String s: list) System.out.println(s);
-        Collections.sort(list, new Comparator<String>(){
+        /*Collections.sort(list, new Comparator<String>(){
              @Override
             public int compare(String o1, String o2) {
                 return list.sort();
             }
-        });
+        });*/
         Comparator<? super String> byLong = new Comparator<String>() {
             @Override
             public int compare(String o1, String o2) {
                 return Integer.compare(o1.length(), o2.length());
             }
         };
-        list.sort();
+
         for (String s: list) System.out.println(s);
 
     }
